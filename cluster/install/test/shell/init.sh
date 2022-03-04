bbf422797d606921b119e4d6d77ad00be3f70ac1
moyu <279972161@qq.com>
2022-03-04 10:29:36 +0800

    在利用helm部署的测试环境中增加了用于远程测试控制的http服务器auto-test-server的部署

--- cluster/install/test/shell/init.sh
+++ cluster/install/test/shell/init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #配置git使用ssh拉取gitee私有仓库代码
 git config --global url."git@gitee.com:".insteadOf https://gitee.com/
-git clone ${PROJECT_REPO_ADDRESS}
-cd ${PROJECT_DIR}
+git clone "${PROJECT_REPO_ADDRESS}" -b "${BRANCH}" --single-branch
+cd "${PROJECT_DIR}" || exit
 #配置go拉取gitee代码时不适用代理
 go env -w GOPRIVATE=gitee.com
 go env -w GOPROXY=https://goproxy.cn,https://goproxy.io,direct
-go test -v -run ^\(${TEST_PREFIX}\) ./...
+go test -v -run ^\("${TEST_PREFIX}"\) -coverprofile coverprofile.out -coverpkg "${TEST_PKG}" ./...
+go tool cover -func=coverprofile.out
+cd .. && rm -rf "${PROJECT_DIR}"
