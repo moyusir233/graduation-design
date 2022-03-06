69202df7666b6fe634000937a74c29d606f891df
moyu <279972161@qq.com>
2022-03-06 22:38:23 +0800

    1. 针对dataCollection的测试，修改了测试环境的部署配置 2. 针对auto-test-server增加的查看测试覆盖报告的功能，修改了其使用的初始化脚本init.sh 3. 修改了测试环境的helm文件的依赖配置，将redis的配置写入了Chart.yaml中

--- cluster/install/test/shell/init.sh
+++ cluster/install/test/shell/init.sh
@@ -3,9 +3,11 @@
 git config --global url."git@gitee.com:".insteadOf https://gitee.com/
 git clone "${PROJECT_REPO_ADDRESS}" -b "${BRANCH}" --single-branch
 cd "${PROJECT_DIR}" || exit
-#配置go拉取gitee代码时不适用代理
+#配置go拉取gitee代码时不使用代理
 go env -w GOPRIVATE=gitee.com
 go env -w GOPROXY=https://goproxy.cn,https://goproxy.io,direct
 go test -v -run ^\("${TEST_PREFIX}"\) -coverprofile coverprofile.out -coverpkg "${TEST_PKG}" ./...
 go tool cover -func=coverprofile.out
+go tool cover -html=coverprofile.out -o report.html
+mv report.html ..
 cd .. && rm -rf "${PROJECT_DIR}"
