a019622ee9183a8a0a65b903e0c3415032fcb29a
moyu <279972161@qq.com>
2022-03-09 20:52:24 +0800

    1. 修改了auto-test-server使用的测试脚本，将覆盖测试改成了由环境变量控制的可选功能 2. 为auto-test-server增加了readiness探针

--- cluster/install/test/shell/test.sh
+++ cluster/install/test/shell/test.sh
@@ -3,8 +3,12 @@
 
 git clone "${PROJECT_REPO_ADDRESS}" -b "${BRANCH}" --single-branch
 cd "${PROJECT_DIR}" || exit
-go test -v -timeout "${TEST_TIMEOUT}" -run ^\("${TEST_PREFIX}"\) -coverprofile coverprofile.out -coverpkg "${TEST_COVER_PKG}" ./...
-go tool cover -func=coverprofile.out
-go tool cover -html=coverprofile.out -o report.html
-mv report.html ..
+if [ "${TEST_COVER_ENABLE}" == "enable" ]; then
+  go test -v -timeout "${TEST_TIMEOUT}" -run ^\("${TEST_PREFIX}"\) -coverprofile coverprofile.out -coverpkg "${TEST_COVER_PKG}" ./...
+  go tool cover -func=coverprofile.out
+  go tool cover -html=coverprofile.out -o report.html
+  mv report.html ..
+else
+  go test -v -timeout "${TEST_TIMEOUT}" -run ^\("${TEST_PREFIX}"\) ./...
+fi
 cd .. && rm -rf "${PROJECT_DIR}"
