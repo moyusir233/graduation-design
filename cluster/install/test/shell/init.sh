6d2522e8d374d99dddc8fe3d8c9e60ce9de92033
moyu <279972161@qq.com>
2022-03-09 15:31:50 +0800

    利用go的dlv库(https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_test.md)以及gland IDE的go remote功能，为auto-test-server添加了远程debug的功能

--- cluster/install/test/shell/init.sh
+++ cluster/install/test/shell/init.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#配置git使用ssh拉取gitee私有仓库代码
-git config --global url."git@gitee.com:".insteadOf https://gitee.com/
-git clone "${PROJECT_REPO_ADDRESS}" -b "${BRANCH}" --single-branch
-cd "${PROJECT_DIR}" || exit
+#该脚本用于初始化auto-test-server的环境
+
 #配置go拉取gitee代码时不使用代理
 go env -w GOPRIVATE=gitee.com
+#配置go拉取除了gitee以外的代码时使用代理
 go env -w GOPROXY=https://goproxy.cn,https://goproxy.io,direct
-go test -v -run ^\("${TEST_PREFIX}"\) -coverprofile coverprofile.out -coverpkg "${TEST_PKG}" ./...
-go tool cover -func=coverprofile.out
-go tool cover -html=coverprofile.out -o report.html
-mv report.html ..
-cd .. && rm -rf "${PROJECT_DIR}"
+#配置git使用ssh拉取gitee私有仓库代码
+git config --global url."git@gitee.com:".insteadOf https://gitee.com/
+#下载开启远程debug的dlv程序
+go install github.com/go-delve/delve/cmd/dlv@latest
---
+++ cluster/install/test/shell/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#该脚本用于拉取git仓库代码，并依据环境变量执行go test
+
+git clone "${PROJECT_REPO_ADDRESS}" -b "${BRANCH}" --single-branch
+cd "${PROJECT_DIR}" || exit
+go test -v -timeout "${TEST_TIMEOUT}" -run ^\("${TEST_PREFIX}"\) -coverprofile coverprofile.out -coverpkg "${TEST_COVER_PKG}" ./...
+go tool cover -func=coverprofile.out
+go tool cover -html=coverprofile.out -o report.html
+mv report.html ..
+cd .. && rm -rf "${PROJECT_DIR}"
